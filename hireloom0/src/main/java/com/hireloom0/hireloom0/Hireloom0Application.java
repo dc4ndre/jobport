bbb2f33f7e19a92471f0383fb02af5eae7102365
dc4ndre <mhyrrandread@gmail.com>
2025-03-17 15:51:32 +0800

    Initial commit

---
+++ hireloom0/src/main/java/com/hireloom0/hireloom0/Hireloom0Application.java
@@ -0,0 +1,13 @@
+package com.hireloom0.hireloom0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hireloom0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hireloom0Application.class, args);
+	}
+
+}
---
+++ hireloom0/src/test/java/com/hireloom0/hireloom0/Hireloom0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireloom0.hireloom0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hireloom0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
