32fc489cea6f0c9e39a9dbbfde112a9c21ebbb98
dc4ndre <mhyrrandread@gmail.com>
2025-03-17 12:52:59 +0800

    Initial commit

---
+++ Hireloom/src/main/java/com/jobport/Hireloom/HireloomApplication.java
@@ -0,0 +1,13 @@
+package com.jobport.Hireloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireloomApplication.class, args);
+	}
+
+}
